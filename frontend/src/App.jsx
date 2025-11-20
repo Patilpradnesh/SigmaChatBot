8d295ca64f4ad541ba75273e393aa75525f47651
patilpradnesh <patilpradnesh4124@gmail.com>
2025-11-20 15:42:07 +0530

    import fixing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import Navbar from "./components/Navbar";
-import Chatbot from "./components/Chatbot";
+import Chatbot from "./components/ChatBot";
 
 export default function App() {
   return (
