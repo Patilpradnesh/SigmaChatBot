a4a731e4275bb8caa43041c51159d3c98d4f3562
patilpradnesh <patilpradnesh4124@gmail.com>
2025-11-20 14:55:27 +0530

    Update settings.py

--- backend/realestate/settings.py
+++ backend/realestate/settings.py
@@ -120,13 +120,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
